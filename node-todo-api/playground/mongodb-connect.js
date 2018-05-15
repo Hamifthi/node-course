116dd5022a1edc42a45a1db17970f441ed91fdda
Hamifthi <hamedonil@yahoo.com>
2018-05-15 19:04:45 +0430

    add post /todo route and modifying mongoose

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,37 +1,35 @@
 // const MongoClient = require('mongodb').MongoClient;
-const {MongoClient, ObjectID} = require('mongodb')
+const {MongoClient, ObjectID} = require('mongodb');
 
-// let objectid = new ObjectID();
-// console.log(objectid)
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
-    if (error) {
-        return console.log('Unable to connect to MongoDB server')
-    }
-    console.log('connect to MongoDB server')
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
 
-    // db.collection('Todos').insertOne({
-    //     text: 'sth here',
-    //     completed: false
-    // }, (error, result) => {
-    //     if (error) {
-    //         return console.log('Unabale to insert todos', error)
-    //     }
+  // Insert new doc into Users (name, age, location)
+  // db.collection('Users').insertOne({
+  //   name: 'Andrew',
+  //   age: 25,
+  //   location: 'Philadelphia'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
-    //     console.log(JSON.stringify(result.ops, undefined, 2))
-    // });
-
-    db.collection('Users').insertOne({
-        name: 'Hamed Fathi',
-        age: 21,
-        location: 'Karaj'
-    }, (error, result) => {
-        if (error) {
-            return console.log('Unabale to insert todos', error)
-        }
-        console.log(result)
-        console.log(JSON.stringify(result.ops, undefined, 2))
-    });
-
-    db.close()
+  db.close();
 });
--- node-todo-api/playground/mongodb-update.js
+++ node-todo-api/playground/mongodb-update.js
@@ -1,37 +1,38 @@
-const {MongoClient, ObjectID} = require('mongodb')
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
-    if (error) {
-        return console.log('Unable to connect to MongoDB server')
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('57bc4b15b3b6a3801d8c47a2')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('57abbcf4fd13a094e481cf2c')
+  }, {
+    $set: {
+      name: 'Andrew'
+    },
+    $inc: {
+      age: 1
     }
-    console.log('connect to MongoDB server')
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
 
-    // db.collection('Todos').findOneAndUpdate({
-    //     _id: new ObjectID("5af85a151a0786208b92ade9")
-    // }, {
-    //     $set: {
-    //         completed: true
-    //     }
-    // }, {
-    //     returnOriginal: false
-    // }).then((result) => {
-    //     console.log(result)
-    // })
-
-    db.collection('Users').findOneAndUpdate({
-        _id: new ObjectID("5af6f91088b85302a8645458")
-    }, {
-        $set: {
-            name: 'Hamed Fathi'
-        },
-        $inc: {
-            age: 1
-        }
-    }, {
-        returnOriginal: false
-    }).then((result) => {
-        console.log(result)
-    })
-
-    db.close();
+  // db.close();
 });
