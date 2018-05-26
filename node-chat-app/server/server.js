d047e2ecf6d8f1e499a3bfd19a0bc129feab41e3
Hamifthi <hamedonil@yahoo.com>
2018-05-26 11:09:05 +0430

    Add connect and disconnect event handler

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -1,12 +1,25 @@
 const path = require('path');
-const express = require('express')
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected from server')
+      })
+});
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log(`Server run on port ${port}`);
 })
