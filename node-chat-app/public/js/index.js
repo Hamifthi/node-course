2805527439b2e2f7e5b0a203b5319d8a074fe27e
Hamifthi <hamedonil@yahoo.com>
2018-05-27 15:04:03 +0430

    Add geolocation support via geolocation api

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -10,12 +10,22 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-    console.log('New Message', message)
-    var li = jQuery('<li></li>')
-    li.text(`${message.from}: ${message.text}`)
+    console.log('New Message', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
 
-    jQuery('#messages').append(li)
+    jQuery('#messages').append(li);
 });
+
+socket.on('newLocationMessage', function (message) {
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My current location</a>');
+
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+})
 
 jQuery('#message-form').on('submit', function (event) {
   event.preventDefault()
@@ -25,6 +35,22 @@ jQuery('#message-form').on('submit', function (event) {
     text: jQuery('[name=message]').val()
   }, function () {
 
-  })
-})
+  });
+});
 
+let locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if(!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser')
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    console.log(position);
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    alert('Unable to fetch location')
+  });
+});
--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 
-const generateMessage = require('./utils/message')
+const {generateMessage, generateLocationMessage} = require('./utils/message')
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -22,17 +22,11 @@ io.on('connection', (socket) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text))
         callback('This is a message from server');
-        // io.emit('newMessage', {
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // })
-        // socket.broadcast.emit('newMessage', {
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // })
     });
+
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
+    })
 
     socket.on('disconnect', () => {
         console.log('User disconnected from server')
