0778786aa8dcb33e25da0bf9a742db5d79932ba3
Hamifthi <hamedonil@yahoo.com>
2018-05-27 16:07:51 +0430

    Format timestamps using moment js

---
+++ node-chat-app/playground/time.js
@@ -0,0 +1,7 @@
+const moment = require('moment');
+
+// let date = new moment();
+// console.log(date.format('MMM Do YYYY'))
+
+let date = new moment();
+console.log(date.format('h:mm a'))
--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -1,56 +1,61 @@
 var socket = io();
 
 socket.on('connect', function () {
-  console.log('connected to the server');
-  
+  console.log('Connected to server');
 });
 
 socket.on('disconnect', function () {
-  console.log('Disconnedcted from server')
+  console.log('Disconnected from server');
 });
 
 socket.on('newMessage', function (message) {
-    console.log('New Message', message);
-    var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+  let formattedTime = moment(message.createdAt).format('h:mm a')
+  let li = jQuery('<li></li>');
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
-    jQuery('#messages').append(li);
+  jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location</a>');
+  let li = jQuery('<li></li>');
+  let a = jQuery('<a target="_blank">My current location</a>');
+  let formattedTime = moment(message.createdAt).format('h:mm a')
 
-    li.text(`${message.from}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
-})
+  li.text(`${message.from} ${formattedTime}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
 
-jQuery('#message-form').on('submit', function (event) {
-  event.preventDefault()
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  let messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-
+    messageTextbox.val('')
   });
 });
 
-let locationButton = jQuery('#send-location');
+var locationButton = jQuery('#send-location');
 locationButton.on('click', function () {
-  if(!navigator.geolocation) {
-    return alert('Geolocation not supported by your browser')
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
-    console.log(position);
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function () {
-    alert('Unable to fetch location')
+    locationButton.removeAttr('disabled').text('Send location');
+    alert('Unable to fetch location.');
   });
 });
